8494e045f5c204be3d2b8ba0269427b8fd8c1035
Giulia Marcela <giuliamendes67@gmail.com>
2025-03-25 21:30:36 -0300

    feat: adiciona alterações da nova tela principal

--- app/src/main/java/com/example/hamburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -1,48 +1,43 @@
 package com.example.hamburgueriaz;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hamburgueriaz.adapter.BurgerAdapter;
+import com.example.hamburgueriaz.model.Burger;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textViewQuantity;
-    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        List<Burger> burgers = new ArrayList<>();
+        burgers.add(new Burger("Egg X-Burger", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in fermentum ex.", "R$ 20.00", R.drawable.unsplash_burger_photo));
+        burgers.add(new Burger("Cheese Burger", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in fermentum ex.", "R$ 20.00", R.drawable.unsplash_burger_photo));
+        burgers.add(new Burger("Chicken Burger", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in fermentum ex.", "R$ 20.00", R.drawable.unsplash_burger_photo));
+        burgers.add(new Burger("Veggie Burger", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in fermentum ex.", "R$ 20.00", R.drawable.unsplash_burger_photo));
+
+        RecyclerView recyclerView = findViewById(R.id.cards_view_rec);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(new BurgerAdapter(burgers));
+
+        MaterialButton submitOrderButton = findViewById(R.id.submit_order_button);
+
+        submitOrderButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, OrderSummaryActivity.class);
+            startActivity(intent);
         });
-
-        textViewQuantity = findViewById(R.id.text_quantity);
-        Button btnDecrease = findViewById(R.id.btn_decrease);
-        Button btnIncrease = findViewById(R.id.btn_increase);
-
-        btnDecrease.setOnClickListener(v -> decreaseQuantity());
-        btnIncrease.setOnClickListener(v -> increaseQuantity());
-    }
-
-    private void decreaseQuantity() {
-        if (quantity > 1) {
-            quantity--;
-            textViewQuantity.setText(String.valueOf(quantity));
-        }
-    }
-
-    private void increaseQuantity() {
-        quantity++;
-        textViewQuantity.setText(String.valueOf(quantity));
     }
 }
