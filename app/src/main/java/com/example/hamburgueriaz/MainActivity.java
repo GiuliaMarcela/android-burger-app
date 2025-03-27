c882f92a49e36a4894369d960f8a94e4a406b596
Giulia Marcela <giuliamendes67@gmail.com>
2025-03-26 21:59:17 -0300

    feat: adiciona funcionalidade de selecionar hamburguer e respectivas estilizações

--- app/src/main/java/com/example/hamburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.hamburgueriaz;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Burger selectedBurger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +33,29 @@ public class MainActivity extends AppCompatActivity {
         burgers.add(new Burger("Chicken Burger", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in fermentum ex.", "R$ 20.00", R.drawable.unsplash_burger_photo));
         burgers.add(new Burger("Veggie Burger", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in fermentum ex.", "R$ 20.00", R.drawable.unsplash_burger_photo));
 
-        RecyclerView recyclerView = findViewById(R.id.cards_view_rec);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        recyclerView.setAdapter(new BurgerAdapter(burgers));
-
         MaterialButton submitOrderButton = findViewById(R.id.submit_order_button);
+        RecyclerView recyclerView = findViewById(R.id.cards_view_rec);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        BurgerAdapter adapter = new BurgerAdapter(burgers, burger -> {
+            selectedBurger = burger;
+            submitOrderButton.setEnabled(true);
+            submitOrderButton.setBackgroundTintList(
+                    ColorStateList.valueOf(
+                            ContextCompat.getColor(this, R.color.accent_red)
+                    )
+            );
+            submitOrderButton.postInvalidate();
+        });
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(adapter);
+        submitOrderButton.setEnabled(false);
 
         submitOrderButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, OrderSummaryActivity.class);
+            //TODO: Passar dados para a próxima tela
             startActivity(intent);
         });
     }
--- app/src/main/java/com/example/hamburgueriaz/adapter/BurgerAdapter.java
+++ app/src/main/java/com/example/hamburgueriaz/adapter/BurgerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.hamburgueriaz.adapter;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,35 +8,52 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hamburgueriaz.R;
 import com.example.hamburgueriaz.model.Burger;
+import com.google.android.material.card.MaterialCardView;
 
 import java.util.List;
 
 public class BurgerAdapter extends RecyclerView.Adapter<BurgerAdapter.BurgerViewHolder> {
-    private List<Burger> burgers;
+    private final List<Burger> burgers;
+    private final OnItemClickListener onItemClickListener;
+    private int selectedPosition = -1;
 
-    public BurgerAdapter(List<Burger> burgers) {
+    public BurgerAdapter(List<Burger> burgers, OnItemClickListener onItemClickListener) {
         this.burgers = burgers;
+        this.onItemClickListener = onItemClickListener;
     }
 
     @NonNull
     @Override
     public BurgerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_burger_card, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.item_burger_card,
+                parent,
+                false
+        );
         return new BurgerViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull BurgerViewHolder holder, int position) {
         Burger burger = burgers.get(position);
-        holder.burgerName.setText(burger.getName());
-        holder.burgerImage.setImageResource(burger.getImageLocation());
-        holder.burgerDescription.setText(burger.getDescription());
-        holder.burgerPrice.setText(burger.getPrice());
+
+        holder.bind(burger, holder.getAdapterPosition() == selectedPosition);
+
+        holder.itemView.setOnClickListener(v -> {
+            int previousSelected = selectedPosition;
+            selectedPosition = holder.getAdapterPosition();
+
+            if (previousSelected != RecyclerView.NO_POSITION) {
+                notifyItemChanged(previousSelected);
+            }
+            notifyItemChanged(selectedPosition);
+            onItemClickListener.onItemClick(burgers.get(selectedPosition));
+        });
     }
 
     @Override
@@ -43,18 +61,44 @@ public class BurgerAdapter extends RecyclerView.Adapter<BurgerAdapter.BurgerView
         return burgers.size();
     }
 
+    public interface OnItemClickListener {
+        void onItemClick(Burger burger);
+    }
+
     public static class BurgerViewHolder extends RecyclerView.ViewHolder {
+        MaterialCardView cardView;
         ImageView burgerImage;
-        TextView burgerName;
-        TextView burgerDescription;
-        TextView burgerPrice;
+        TextView burgerName, burgerDescription, burgerPrice;
 
         public BurgerViewHolder(View itemView) {
             super(itemView);
+            cardView = itemView.findViewById(R.id.card_view);
             burgerImage = itemView.findViewById(R.id.card_view_image);
             burgerName = itemView.findViewById(R.id.card_view_title);
             burgerDescription = itemView.findViewById(R.id.card_view_description);
             burgerPrice = itemView.findViewById(R.id.card_view_price);
+        }
+
+        public void bind(Burger burger, boolean isSelected) {
+            Context context = itemView.getContext();
+            burgerName.setText(burger.getName());
+            burgerImage.setImageResource(burger.getImageLocation());
+            burgerDescription.setText(burger.getDescription());
+            burgerPrice.setText(burger.getPrice());
+
+            cardView.setStrokeColor(
+                    ContextCompat.getColor(
+                            itemView.getContext(),
+                            isSelected ? R.color.selected : R.color.primary_dark
+                    )
+            );
+            cardView.setCardBackgroundColor(ContextCompat.getColor(
+                            context,
+                            isSelected ? R.color.selected_background : R.color.transparent
+                    )
+            );
+            cardView.setStrokeWidth(isSelected ? 8 : 1);
+            cardView.postInvalidate();
         }
     }
 }
